5a2c233cd60c250c1cf49bca71325308ac4bce78
Cyliann <maksymilian@cych.eu>
2025-02-13 13:05:52 +0100

    Add swagger to generate documentation

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,8 @@ import (
 	"github.com/Cyliann/go-dice-roller/internal/server"
 	"github.com/Cyliann/go-dice-roller/internal/token"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func loadRoutes() *gin.Engine {
@@ -17,5 +19,7 @@ func loadRoutes() *gin.Engine {
 	// POST form: { "dice" : "number of sides" }
 	router.POST("/roll", token.Validate(), s.HandleRolls())
 
+	// Docs
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return router
 }
